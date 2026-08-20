19626ed9b909c7c20501ed4b2afa1baacdb1fa8d
Humberto Ortiz-Zuazaga <humberto.ortiz@upr.edu>
2026-08-20 10:34:06 -0400

    Add integer stack example code (and license)

---
+++ int-stack/README.md
@@ -0,0 +1,5 @@
+# Integer stack
+
+Example integer stack, you can compile with:
+
+g++ main.cpp -o main
---
+++ int-stack/main.cpp
@@ -0,0 +1,34 @@
+// main.cpp - tests for integer stack
+// Copyright 2026 Humberto Ortiz Zuazaga
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include <iostream>
+#include "stack.h"
+
+int main() {
+  intstack s;
+
+  s.push(5);
+  s.push(3);
+  std::cout << s.pop() << std::endl;
+  s.pop();
+
+  s.push(99);
+  std::cout << s.pop() << std::endl;
+
+  
+  // s.pop(); // pop de stack vacio causa error
+  return 0;
+}
